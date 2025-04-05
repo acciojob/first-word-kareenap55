d8b1c1e0941727bccda48148f2542a33fa51d3a3
kareenap55 <kareenap1412@gmail.com>
2025-04-05 02:18:06 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function firstWord(s) {
   // your code here
+	function firstWord(str) {
+    // Trim leading spaces to make sure we get the actual first word
+    str = str.trimStart();
+
+    // Find the index of the first space
+    const spaceIndex = str.indexOf(' ');
+
+    // If there's no space, return the whole string
+    if (spaceIndex === -1) {
+        return str;
+    }
+
+    // Otherwise, return the substring up to the first space
+    return str.substring(0, spaceIndex);
+}
 }
 
 // Do not change the code below
