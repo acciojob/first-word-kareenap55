24b36e8a9c260ad9658319eb8cc554d9d3b8369a
kareenap55 <kareenap1412@gmail.com>
2025-04-05 02:34:19 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,15 @@
-function firstWord(s) {
-  // your code here
-	function firstWord(str) {
-  // Remove leading spaces
+function firstWord(str) {
+  // Trim leading spaces to handle cases like ' Hello'
   str = str.trimStart();
-
+  
   // Find the index of the first space
   const spaceIndex = str.indexOf(' ');
 
-  // If no space is found, return the whole string
+  // If no space found, return the whole string
   if (spaceIndex === -1) {
     return str;
   }
 
-  // Return the substring up to the first space
+  // Return substring from beginning to first space
   return str.substring(0, spaceIndex);
 }
-}
-
-// Do not change the code below
-
-const s = prompt("Enter String:");
-alert(firstWord(s));
